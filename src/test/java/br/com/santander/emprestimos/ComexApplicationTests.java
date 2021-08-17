4dd2f05cd4f919faf2099a9bb18658b89e84cf92
Samuel Costa <samuelmfa@gmail.com>
2021-08-17 20:18:11 -0300

    primeiro commit

---
+++ src/test/java/br/com/santander/emprestimos/ComexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.emprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
